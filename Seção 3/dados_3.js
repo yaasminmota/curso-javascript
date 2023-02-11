950ca025627309df4e1619d20442d67d6a3554f5
Yasmin Mota <124382453+yaasminmota@users.noreply.github.com>
2023-02-10 21:57:07 -0300

    Atualizado em 10/02/2023

---
+++ Seção 3/dados_3.js
@@ -0,0 +1,11 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4.280;
+console.log(typeof salario);
+
+let estaChovendo = false;
+console.log(typeof estaChovendo);
+
+console.log(typeof 'Teste');
---
+++ Seção 3/desafio_area.js
@@ -0,0 +1,12 @@
+// Área da circunferência
+
+const PI = 3.141592;
+const raio = 10;
+let area  = PI * raio * raio;
+
+console.log('A área da circunferência é ' + area 
++ ' m2.');
+
+area = Math.PI * raio * raio;
+console.log('A área da circunferência é ' + area 
++ ' m2.');
