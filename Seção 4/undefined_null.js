93d1fa7108f51b2a8e2e3cec2eb7f55522d17072
Yasmin Mota <124382453+yaasminmota@users.noreply.github.com>
2023-02-13 21:57:20 -0300

    Atualizado em 13/02/2023

---
+++ Seção 4/undefined_null.js
@@ -0,0 +1,12 @@
+//Undefined
+let valor // não inicializada
+console.log(valor)
+
+//Null
+valor = null // ausência de valor
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco) //undefined
+
+prod
