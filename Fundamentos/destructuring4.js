54a115e6af188a621ee4b6e48e05dbebef66e049
Yasmin Mota <124382453+yaasminmota@users.noreply.github.com>
2023-02-18 23:41:27 -0300

    Atualizado em 18/02/2023

--- Fundamentos/destructuring4.js
+++ Fundamentos/destructuring4.js
@@ -6,4 +6,4 @@ function rand ([min = 0, max = 1000]){
 
 console.log(rand([50, 40]))
 console.log(rand([992]))
-console.log(rand([, 10]))
+console.log(rand([, 10 ]))
---
+++ Fundamentos/operadoresRelacionais.js
@@ -0,0 +1,4 @@
+
+console.log('1'== 1)
+// a string '1' é estritamente igual ao number 1?
+console.log('1' === 1)
