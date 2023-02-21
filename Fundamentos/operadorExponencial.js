60b180aa6205fc48bd22dbda81d6ab46b90a7df7
Yasmin Mota <124382453+yaasminmota@users.noreply.github.com>
2023-02-20 22:34:17 -0300

    Atualizado em 20/02/2023

---
+++ Fundamentos/operadorExponencial.js
@@ -0,0 +1,16 @@
+// Exponenciação usando uma função do obj Math
+let exp = Math.pow(2, 3)
+console.log(exp)
+
+// Utilizando o operador próprio
+let exp1 = 2 ** 3
+console.log(exp1)
+
+// Elevando ao cubo
+let base = 2
+base **= 3
+console.log(base)
+ 
+// Expoente negativo
+let exp2 = 2** -3
+console.log(exp)
